3615a47f98a3e09f00d3bf7e639273bc0ed4615b
Daniel Melo <danielfarias380@gmail.com>
2020-08-19 12:23:23 -0300

    Estrutura Inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Filtros from './components/Filtros'
+import Home from './components/Home'
+import Carrinho from './components/Carrinho'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Filtros />
+  
+        <Home />
+  
+        <Carrinho />
+      </div>
+    );
+  }
 }
 
 export default App;
