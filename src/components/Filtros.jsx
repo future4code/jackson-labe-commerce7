bc4fbb27c25afb32720257f745518033f7265a58
Daniel Melo <danielfarias380@gmail.com>
2020-08-19 15:31:27 -0300

    Daniel: Criação do componente Filtros

--- src/components/Filtros.jsx
+++ src/components/Filtros.jsx
@@ -1,9 +1,39 @@
 import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+
+  h2 {
+    margin: 5px 0px;
+  }
+` 
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`
 
 class Filtros extends Component {
   render() {
     return (
-      <h1>Filtros</h1>
+      <StyledDiv>
+        <h2>Filtros</h2>
+
+        <StyledForm>
+            <label htmlFor="">Valor Máximo:</label>
+            <input type="number"/>
+          
+            <label htmlFor="">Valor Mínimo:</label>
+            <input type="number"/>
+          
+            <label htmlFor="">Buscar Produto:</label>
+            <input type="text"/>
+        </StyledForm>
+
+      </StyledDiv>
+      
     )
   }
 }
